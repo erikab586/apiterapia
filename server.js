95ea9f3be44cc10cdae32fb94100b52d3acfca08
erikab586 <erikab644@gmail.com>
2026-04-21 17:47:16 -0300

    el principal

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`🚀 Servidor corriendo en https://apisterapia.onrender.com`);
+  console.log(`🚀 Servidor corriendo en https://apiterapia.onrender.com`);
 });
